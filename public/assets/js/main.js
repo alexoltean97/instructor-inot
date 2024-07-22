35c63e3e8b128259b37dd654949306550ac3e837
alexoltean97 <alex.oltean97@gmail.com>
2024-07-22 12:20:22 +0300

    More work

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,27 +1,85 @@
 $(document).ready(function () {
+  
+  const toggleClass = (element, addClass, removeClass) => {
+      element.addClass(addClass).removeClass(removeClass);
+  };
 
-    // Refactor -- CREATE UTILITY 
-    if ($(window).width() <= 767) {
-        $('h1.ftsz-50.pb-2').each(function() {
-            $(this).removeClass('ftsz-50').addClass('ftsz-24');
-        });
 
-        $('p.ftsz-20').each(function() {
-            $(this).removeClass('ftsz-20').addClass('ftz-15');
-        });
+  const toggleLayout = (isMobileOrTablet) => {
+      const accordionBodies = $(".accordion-body");
+      if (isMobileOrTablet) {
+          accordionBodies.addClass("flex-column");
+      } else {
+          accordionBodies.removeClass("flex-column");
+      }
+  };
 
-    }
+  const toggleFontSizes = (isMobile) => {
+      const mainHeadings = $("h1.ftsz-50.pb-2, h1.ftsz-24.pb-2");
+      const paragraphs = $("p.ftsz-20, p.ftz-15");
 
-    $(window).resize(function() {
-        if ($(window).width() <= 767) {
-            $('h1.ftsz-50.pb-2').each(function() {
-                $(this).removeClass('ftsz-50').addClass('ftsz-24');
-            });
-        } else {
-            $('h1.ftsz-24.pb-2').each(function() {
-                $(this).removeClass('ftsz-24').addClass('ftsz-50');
-            });
-        }
-    });
+      if (isMobile) {
+          mainHeadings.each(function () {
+              toggleClass($(this), "ftsz-24", "ftsz-50");
+          });
+          paragraphs.each(function () {
+              toggleClass($(this), "ftz-15", "ftsz-20");
+          });
+      } else {
+          mainHeadings.each(function () {
+              toggleClass($(this), "ftsz-50", "ftsz-24");
+          });
+          paragraphs.each(function () {
+              toggleClass($(this), "ftsz-20", "ftz-15");
+          });
+      }
+  };
 
+  const checkWindowSize = () => {
+      const width = $(window).width();
+      const isMobile = width <= 767;
+      const isTablet = width > 767 && width <= 992;
+      const isDesktop = width > 992;
+
+      toggleFontSizes(isMobile);
+
+      if (isMobile) {
+          $('.desktop-navigation').addClass('d-none');
+          $('.mobile-navigation').removeClass('d-none');
+          $('.schedule').addClass('text-center');
+          toggleLayout(true); 
+      } else if (isTablet) {
+          $('.desktop-navigation').addClass('d-none');
+          $('.mobile-navigation').removeClass('d-none');
+          $('.schedule').addClass('text-center');
+          toggleLayout(true); 
+      } else {
+          $('.desktop-navigation').removeClass('d-none');
+          $('.mobile-navigation').addClass('d-none');
+          $('.schedule').removeClass('text-center');
+          toggleLayout(false);
+      }
+  };
+
+  $(document).ready(checkWindowSize);
+  $(window).resize(checkWindowSize);
+
+  const toggleMenu = (showMenu) => {
+      const mobileNav = $(".mobile-nav");
+      const openBtn = $(".btn_open");
+      const closeBtn = $(".btn_close");
+
+      if (showMenu) {
+          toggleClass(mobileNav, "d-block", "d-none");
+          toggleClass(openBtn, "d-none", "d-block");
+          toggleClass(closeBtn, "d-block", "d-none");
+      } else {
+          toggleClass(mobileNav, "d-none", "d-block");
+          toggleClass(openBtn, "d-block", "d-none");
+          toggleClass(closeBtn, "d-none", "d-block");
+      }
+  };
+
+  $(".btn_open").on("click", () => toggleMenu(true));
+  $(".btn_close").on("click", () => toggleMenu(false));
 });
